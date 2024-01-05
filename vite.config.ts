38302cf3dc16420921308260b4eb50310952dc01
dr666m1 <skndr666m1@gmail.com>
2024-01-06 07:56:54 +0900

    generate manifest.json in vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { defineManifest } from "@crxjs/vite-plugin";
+import { crx } from "@crxjs/vite-plugin";
+
+import packageJson from "./package.json";
+
+const manifest = defineManifest(async () => ({
+  manifest_version: 3,
+  name: "BigQuery Confirm Dialog",
+  version: packageJson.version,
+  action: {
+    default_popup: "index.html",
+  },
+}));
+
+export default defineConfig({
+  plugins: [crx({ manifest })],
+});
