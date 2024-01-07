3ba9c9d3c86e1561456d77c143cf04b4fd1abc2f
dr666m1 <skndr666m1@gmail.com>
2024-01-08 00:00:44 +0900

    fix manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
+import { crx, defineManifest, ManifestV3Export } from "@crxjs/vite-plugin";
+import package_ from "./package.json";
+
+const manifest: ManifestV3Export = defineManifest(async () => ({
+  manifest_version: 3,
+  name: package_.name.replace(/-/g, " "),
+  version: package_.version,
+  host_permissions: ["file:///*"],
+}));
 
 // https://vitejs.dev/config/
 export default defineConfig({
