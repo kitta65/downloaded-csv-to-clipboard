8f13365e7a44b508f51cc7f1002dd3e8536d2985
dr666m1 <skndr666m1@gmail.com>
2024-01-07 23:38:32 +0900

    re:initialize (npm init vite@latest)

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,7 @@
-import { defineConfig } from "vite";
-import { ManifestV3Export, defineManifest, crx } from "@crxjs/vite-plugin";
-
-import package_ from "./package.json";
-
-const manifest: ManifestV3Export = defineManifest(async () => ({
-  manifest_version: 3,
-  name: "Downloaded CSV to Clipboard",
-  version: package_.version,
-  permissions: ["downloads"],
-  background: {
-    service_worker: "src/background.ts",
-  },
-  host_permissions: ["file:///*"],
-  content_scripts: [
-    {
-      js: ["src/content.ts"],
-      matches: ["<all_urls>"],
-    },
-  ],
-}));
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [crx({ manifest })],
-  // https://github.com/crxjs/chrome-extension-tools/issues/696#issuecomment-1526138970
-  server: {
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      port: 5173,
-    },
-  },
-});
+  plugins: [react()],
+})
