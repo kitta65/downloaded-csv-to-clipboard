5fb8b9cc64f0f2fa429f7838105418d1498e0f15
dr666m1 <skndr666m1@gmail.com>
2024-01-08 10:17:46 +0900

    add test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -2,7 +2,13 @@ import { describe, expect, test } from "vitest";
 import { csv2tsv } from "./utils";
 
 describe("csv2tsv", () => {
-  test("simple csv", () => {
-    expect(csv2tsv("a,b\n1,2")).toBe("a\tb\n1\t2");
+  test("simple", () => {
+    expect(csv2tsv("a,b\n1,2\n3,4")).toBe("a\tb\n1\t2\n3\t4");
+  });
+  test("hiragana", () => {
+    expect(csv2tsv("あ,い\nう,え")).toBe("あ\tい\nう\tえ");
+  });
+  test("quoted", () => {
+    expect(csv2tsv('"a","b"\n"1","2"')).toBe("a\tb\n1\t2"); // should be unquoted
   });
 });
